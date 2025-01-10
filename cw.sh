3d838f6556ba30d629148b783fd448776f3bdfa7
gitpushjoe <104105011+gitpushjoe@users.noreply.github.com>
2025-01-10 11:49:10 -0500

    Initial commit

--- cw.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Get the absolute path of the directory containing the current script
-SCRIPT_DIR=$(dirname "$(realpath "$0")")
-
-# Run the Lua script located in the same directory as the current script
-lua "$SCRIPT_DIR/main.lua" "$@"
---
+++ lua/crazywall/init.lua
@@ -0,0 +1,7 @@
+local M = {}
+
+M.setup = function()
+	print("Hi")
+end
+
+return M
